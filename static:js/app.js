ef5a9f05c1df8481de5170e55e39ff20bcff5f94
spennington <spennington13@gmail.com>
2021-01-13 15:45:39 -0500

    debugging app.js
    not making much progress.  Need to consult class notes.

--- static:js/app.js
+++ static:js/app.js
@@ -3,8 +3,8 @@ function buildData(sample) {
   d3.json("samples.json").then(sampledata =>{
           console.log(sampledata);
        })
-  //  var metadata = sampledata.metadata;
-  //  var results = array[0];
+   var metadata = sampledata.metadata;
+   var results = array[0];
    //make sure # element is correct in index.html
    var metadata_panel = d3.select("#sample-metadata");
    metadata_panel.html("");
@@ -17,9 +17,9 @@ function buildData(sample) {
   });
 }
 // Function to build chart
-
+// removed arrow after sampledata closed parenthesis
 function charts(sample){
-  d3.json("samples.json").then(sampledata) => {
+  d3.json("samples.json").then(sampledata)
     var samples = sampledata.samples;
     var results = array [0];
     var ot_ids = results.otu_ids;
